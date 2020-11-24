b8c3b54bb04ea5efa8f5ddacf588b4cc880b7195
Irina Fylyppova (via Travis CI) <irina.h.fylyppova@gmail.com>
2020-11-24 12:43:15 +0000

    Deploy garevna/dgtek-buildings-api to github.com/garevna/dgtek-buildings-api.git:gh-pages

---
+++ precache-manifest.d6a6845c3c78b9d8e0f6248036a5ac1e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5681ecbff04c3380f84647255a547b",
+    "url": "/dgtek-buildings-api/android-chrome-192x192.png"
+  },
+  {
+    "revision": "408c189194f146d27e526f0c08cb02d1",
+    "url": "/dgtek-buildings-api/android-chrome-512x512.png"
+  },
+  {
+    "revision": "7d13462d8387c50a1d957b56c87e9058",
+    "url": "/dgtek-buildings-api/apple-touch-icon.png"
+  },
+  {
+    "revision": "4468d26073d5845db9b6",
+    "url": "/dgtek-buildings-api/css/app.d8d6b012.css"
+  },
+  {
+    "revision": "a5455e6e5ec428842091",
+    "url": "/dgtek-buildings-api/css/chunk-vendors.ddb12dbd.css"
+  },
+  {
+    "revision": "6de038a7ee6eb1278cb13ae663c52c7f",
+    "url": "/dgtek-buildings-api/favicon-16x16.png"
+  },
+  {
+    "revision": "654f0af686d9d3c49692977f38ea07f7",
+    "url": "/dgtek-buildings-api/favicon-32x32.png"
+  },
+  {
+    "revision": "8c9507b0fd21bd24e05ad73377b2c381",
+    "url": "/dgtek-buildings-api/index.html"
+  },
+  {
+    "revision": "4468d26073d5845db9b6",
+    "url": "/dgtek-buildings-api/js/app.217855ef.js"
+  },
+  {
+    "revision": "a5455e6e5ec428842091",
+    "url": "/dgtek-buildings-api/js/chunk-vendors.64f82464.js"
+  },
+  {
+    "revision": "5276c2b1ee003617712f26e2271a41d9",
+    "url": "/dgtek-buildings-api/manifest.json"
+  },
+  {
+    "revision": "912d7b3836eeea0c1b59f7f724cc3270",
+    "url": "/dgtek-buildings-api/mstile-150x150.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dgtek-buildings-api/robots.txt"
+  },
+  {
+    "revision": "cfb9277d45e54434ec544882d9447b5f",
+    "url": "/dgtek-buildings-api/safari-pinned-tab.svg"
+  }
+]);
